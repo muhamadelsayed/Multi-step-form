6efc54ab4267f8e3cf5f6fb84eca6f708fb65dd0
unknown <masalim122006@gmail.com>
2024-02-24 00:49:28 +0200

    finished step 2 with all logic

--- main.js
+++ main.js
@@ -29,13 +29,13 @@ let email = document.querySelector("#emailvalidation");
 let regPhone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
 let regEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
-let requireSpan = document.createElement("span");
-requireSpan.textContent = "This field is required"
-requireSpan.classList.add("requireSpan");
 // let submit1 = document.getElementById("sum1");
 let firstFormData;
 formOne.addEventListener("submit",(e)=>{
     e.preventDefault();
+    let requireSpan = document.createElement("span");
+    requireSpan.textContent = "This field is required"
+    requireSpan.classList.add("requireSpan");
     let firstData = new FormData(e.target);
     let data1 = Object.fromEntries(firstData)
     if(regPhone.test(data1.phone) && data1.name.length>0&&regEmail.test(data1.email)){
@@ -73,13 +73,15 @@ formOne.addEventListener("submit",(e)=>{
 })
 // form one end
 
-// main func
+// main funcs
 function nextStep(){
-    currentStep++
-    let mainArr = [...nextStepEleSBtnS];
+    currentStep++;
     toggelSteps()
 }
-
+function previousStep(){
+    currentStep--;
+    toggelSteps()
+}
 function toggelSteps(){
     for (let i = 1;i<=4; i++){
         if(i !== currentStep){
@@ -95,9 +97,13 @@ function toggelSteps(){
 // step 2 start
 let monthlyEleS = [...document.getElementsByClassName("monthly")];
 let yearlyEleS = [...document.getElementsByClassName("yearly")];
+let plansArr = [...document.getElementById("plans").children];
+
+let secFormData;
 
 let switchContainer = document.getElementById("select-plan-period-container");
 // default
+let monYear = "month";
 yearlyEleS.forEach(e =>{
     e.style.display = "none";
 });
@@ -114,9 +120,10 @@ switchMonYear.addEventListener("change", (e) => {
     yearlyEleS.forEach(e =>{
         e.style.display = "block";
     });
-    [...document.getElementById("plans").children].forEach(e =>{
+    plansArr.forEach(e =>{
         e.style.height = "110%"
     });
+    monYear = "year";
 }else{
     switchContainer.children[0].style.color = "hsl(213, 96%, 18%)";
     switchContainer.children[2].style.color = "hsl(231, 11%, 63%)";
@@ -126,17 +133,46 @@ switchMonYear.addEventListener("change", (e) => {
     monthlyEleS.forEach(e =>{
         e.style.display = "block";
     });
-    [...document.getElementById("plans").children].forEach(e =>{
+    plansArr.forEach(e =>{
         e.style.height = "100%"
 
     });
+    monYear = "month";
    };
 });
-[...document.getElementById("plans").children].forEach(e =>{
+plansArr.forEach(e =>{
     e.addEventListener("click",()=>{
         e.classList.toggle("selectedPlan");
-        [...document.getElementById("plans").children].forEach(ele =>{if(ele !== e){ele.classList.remove("selectedPlan")}})
+        plansArr.forEach(ele =>{if(ele !== e){ele.classList.remove("selectedPlan")}})
     })
 })
-
+// next step
+let requireSpan2 = document.createElement("span");
+    requireSpan2.textContent = "This field is required"
+    requireSpan2.classList.add("requireSpan");
+document.getElementById("sum2").addEventListener("click",()=>{
+    if(plansArr[0].classList.contains("selectedPlan") || plansArr[1].classList.contains("selectedPlan") || plansArr[2].classList.contains("selectedPlan")){
+        plansArr.forEach(e =>{
+            if(e.classList.contains("selectedPlan")){
+                secFormData = {
+                    id:e.id,
+                    monYear:monYear
+                };
+                console.log(secFormData);
+                nextStep()
+            }
+        })
+    }else{
+        requireSpan2.style.display = "block"
+        document.getElementById("requiredid").append(requireSpan2)
+        requireSpan2.style.position = "static";
+        plansArr.forEach(e =>{
+            e.addEventListener("click",()=>{requireSpan2.style.display = "none"})
+        })
+    }
+})
+// previous step
+previousBtnS[0].addEventListener("click",()=>{
+    previousStep()
+})
 // step 2 end
